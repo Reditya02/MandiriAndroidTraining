a0a0dae4d28c17dbd5044a017d6ce676c23b12fc
Reditya02 <putureditya@gmail.com>
2023-12-29 16:42:33 +0700

    day 3 : adding show more on saving

--- app/src/main/java/com/example/mandiriapps/HomeActivity.kt
+++ app/src/main/java/com/example/mandiriapps/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.mandiriapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mandiriapps.adapter.EWalletAdapter
 import com.example.mandiriapps.adapter.SavingAdapter
 import com.example.mandiriapps.databinding.ActivityHomeBinding
@@ -18,23 +21,9 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val eWalletAdapter = EWalletAdapter()
-
-        val dummyEWalletData = mutableListOf(
-            EWalletModel(image = R.drawable.li, balance = 2000000.0, isConnected = true),
-            EWalletModel(image = R.drawable.orang_lari),
-            EWalletModel(image = R.drawable.logo_bank_mandiri, balance = 5600000.0, isConnected = true),
-            EWalletModel(image = R.drawable.li),
-        )
-
         eWalletAdapter.setDataEWallet(dummyEWalletData)
 
         val savingAdapter = SavingAdapter()
-
-        val dummySavingData = listOf(
-            SavingModel(savingName = "Tabungan A", accountNumber = "12345678", image = R.drawable.ic_cropped_card),
-            SavingModel(savingName = "Tabungan B", accountNumber = "1893409", image = R.drawable.ic_cropped_card),
-        )
-
         savingAdapter.setData(dummySavingData)
 
         binding.apply {
@@ -45,7 +34,33 @@ class HomeActivity : AppCompatActivity() {
             }
 
             compSaving.rvSaving.adapter = savingAdapter
+
+            if (savingAdapter.itemCount != dummySavingData.size) {
+                compSaving.btnShowAll.visibility = View.VISIBLE
+            } else {
+                compSaving.btnShowAll.visibility = View.GONE
+            }
+
+            compSaving.btnShowAll.setOnClickListener {
+                compSaving.btnShowAll.visibility = View.GONE
+                savingAdapter.showAll = true
+                compSaving.rvSaving.adapter = savingAdapter
+            }
         }
 
     }
+
+    private val dummyEWalletData = mutableListOf(
+        EWalletModel(image = R.drawable.li, balance = 2000000.0, isConnected = true),
+        EWalletModel(image = R.drawable.orang_lari),
+        EWalletModel(image = R.drawable.logo_bank_mandiri, balance = 5600000.0, isConnected = true),
+        EWalletModel(image = R.drawable.li),
+    )
+
+    private val dummySavingData = listOf(
+        SavingModel(savingName = "Tabungan A", accountNumber = "12345678", image = R.drawable.ic_cropped_card),
+        SavingModel(savingName = "Tabungan B", accountNumber = "1893409", image = R.drawable.ic_cropped_card),
+        SavingModel(savingName = "Tabungan C", accountNumber = "1893409", image = R.drawable.ic_cropped_card),
+        SavingModel(savingName = "Tabungan D", accountNumber = "1893409", image = R.drawable.ic_cropped_card),
+        )
 }
